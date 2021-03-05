a02c65f939061972ead092cdf4796ee5107b4516
Risabh Raj <nitianrisabh12@gmail.com>
2021-03-05 11:43:33 -0700

    Enable filename validity check

--- web-tier/app.py
+++ web-tier/app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,8 +13,12 @@ def index():
         uploaded_images = request.files.getlist('image_file')
 
         for image in uploaded_images:
-            if image.filename != '':
-                image.save(image.filename)
+            image_name = secure_filename(image.filename)
+            if image_name != '':
+                image_ext = os.path.splitext(image_name)[1]
+                if image_ext not in app.config['UPLOAD_EXTENSIONS']:
+                    abort(400)
+                image.save(os.path.join(app.config['UPLOAD_PATH'], image_name))
 
         return redirect(url_for('index'))
 
