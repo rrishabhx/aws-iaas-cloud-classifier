51e5912be7d213aa2bce573c7a11cc044dad4c14
Risabh Raj <nitianrisabh12@gmail.com>
2021-03-08 00:38:44 -0700

    Create a basic prediction response form

--- web-tier/app.py
+++ web-tier/app.py
@@ -1,18 +1,47 @@
+import logging
 import os
-import time
-from aws import constants as const
+from config import *
 from aws import test_s3 as s3
+from aws import msg_queue as mq
+from aws import autoscaler
 from flask import Flask, render_template, request, redirect, url_for, abort
 from werkzeug.utils import secure_filename
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 app = Flask(__name__)
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif']
+app.config['UPLOAD_EXTENSIONS'] = IMAGE_EXTENSIONS
 app.config['UPLOAD_PATH'] = 'uploads'
+
+
+def handle_image_upload(image, image_name):
+    logger.info("Trying to upload image: %s to S3 input bucket: %s", image, INPUT_BUCKET)
+    s3.upload_file_to_s3(image, INPUT_BUCKET)
+
+    logger.info("Sending image name to SQS: %s", image_name)
+    mq.send_message(REQUEST_QUEUE, image_name)
+
+
+def fetch_from_response_queue(image_set):
+    image_predictions = {}
+    while image_set:
+        # To Do: Update request_q with response_q below
+        recvd_msgs = mq.receive_messages(REQUEST_QUEUE, MAX_NUMBER_OF_MSGS_TO_FETCH, WAIT_TIME_SECONDS, False)
+
+        for msg in recvd_msgs:
+            if msg.body in image_set:
+                print(f"Found {msg.body} in image_set")
+                image_predictions[msg.body] = msg.body
+                image_set.remove(msg.body)
+
+    return image_predictions
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        image_set = set()
         uploaded_images = request.files.getlist('image_file')
 
         for image in uploaded_images:
@@ -21,11 +50,16 @@ def index():
                 image_ext = os.path.splitext(image_name)[1]
                 if image_ext not in app.config['UPLOAD_EXTENSIONS']:
                     abort(400)
+                image_set.add(image_name)
+                handle_image_upload(image, image_name)
 
-                print(f"Trying to upload image: {image_name} to S3 bucket: ", const.INPUT_BUCKET)
-                s3.upload_file_to_s3(image, const.INPUT_BUCKET)
+        # logger.info("Starting auto-scaler")
+        # autoscaler.autoscale_based_on_load()
+        #
+        logger.info("Waiting for response from App-Tier")
+        image_predictions = fetch_from_response_queue(image_set)
 
-        return redirect(url_for('index'))
+        return render_template('index.html', preds=image_predictions)
 
     return render_template('index.html')
 
--- web-tier/config.py
+++ web-tier/config.py
@@ -1,7 +1,11 @@
+# Web-Tier
+IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif']
+
 # SQS
 REQUEST_QUEUE = "Request-Queue"
 RESPONSE_QUEUE = "Response-Queue"
-
+MAX_NUMBER_OF_MSGS_TO_FETCH = 5
+WAIT_TIME_SECONDS = 3
 
 # EC2
 AMI_ID = "ami-0ee8cf7b8a34448a6"
@@ -9,7 +13,9 @@ EC2_KEY_PAIR = "rizz_key_pair"
 SECURITY_GROUP_NAME = ["AllowSSH"]
 INSTANCE_TYPE = "t2.micro"
 APP_SERVER_NAME = "App-Server"
-
+MIN_POSSIBLE_INSTANCES = 1
+MAX_POSSIBLE_INSTANCES = 15
+MAX_REQUESTS_PER_INSTANCE = 5
 
 # S3
 INPUT_BUCKET = "cse546-input-bucket"
